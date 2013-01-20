3366860db391de030728e9e760b1272a1b0275ef
gd <gregdzezinski@gmail.com>
2013-01-20 17:56:34 -0500

    Added lightbox for photo gallery.  Created directory structure for manual updates

---
+++ JeffWeb/Jeff.Model/Domain/Gallery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Jeff.Model.Domain
+{
+    public class Gallery
+    {
+        public string Name { get; set; }
+
+        public List<string> FileNames { get; set; }
+    }
+}
---
+++ JeffWeb/JeffWeb/Services/PhotoReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Jeff.Model.Domain;
+
+namespace JeffWeb.Services
+{
+    public class PhotoReader
+    {
+        private static string GALLERY_FOLDER = "~/Content/Media/Photos";
+
+        public List<Gallery> GetGalleries()
+        {
+            var dirInfo = new DirectoryInfo(GALLERY_FOLDER);
+
+            return null;
+        }
+    }
+}
