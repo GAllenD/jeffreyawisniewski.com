c47f8ba65a26b57ffcf3a935e9ae6281fb2e0acb
gd <gregdzezinski@gmail.com>
2013-02-24 09:36:09 -0500

    converting admin items to collection of objects to help maintain id on postback.

--- JeffWeb/Jeff.Model/View/AdminView.cs
+++ JeffWeb/Jeff.Model/View/AdminView.cs
@@ -6,34 +6,27 @@ namespace Jeff.Model.View
     {
         public AdminView()
         {
-            VoiceEntries = new Dictionary<string, string>();
-            VideoEntries = new Dictionary<string, string>();
+            VoiceEntries = new List<AdminItem>();
+            VideoEntries = new List<AdminItem>();
         }
 
         public string EmailAddress { get; set; }
         public string HomeHtml { get; set; }
         public string BioText { get; set; }
-        public Dictionary<string, string> VoiceEntries { get; set; }
-        public Dictionary<string, string> VideoEntries { get; set; }
+        //public Dictionary<string, string> VoiceEntries { get; set; }
+        //public Dictionary<string, string> VideoEntries { get; set; }
+
+        public List<AdminItem> VoiceEntries { get; set; }
+        public List<AdminItem> VideoEntries { get; set; } 
 
         
     }
 
-    public static class AdminItems 
+    public class AdminItem
     {
-
-
-        public static Dictionary<string, string> GetItemEntries(AdminView view)
-        {
-            
-                var entries = view.VoiceEntries;
-
-                foreach (var videoEntry in view.VideoEntries)
-                {
-                    entries.Add(videoEntry.Key, videoEntry.Value);
-                }
-
-                return entries;
-        } 
+        public long PageConfigurationIdentifier { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
     }
+
 }
--- JeffWeb/JeffWeb/Controllers/AdminController.cs
+++ JeffWeb/JeffWeb/Controllers/AdminController.cs
@@ -37,14 +37,26 @@ namespace JeffWeb.Controllers
 
         private AdminView MapToForm()
         {
-            return new AdminView
+            var view = new AdminView
             {
                 EmailAddress = _pageConfiguration.First(p => p.Page == PageType.Contact.ToString()).EmailAddress,
                 HomeHtml = _pageConfiguration.First(p => p.Page == PageType.Home.ToString()).Text,
-                BioText = _pageConfiguration.First(p => p.Page == PageType.Bio.ToString()).Text,
-                VoiceEntries = _pageConfiguration.Where(p => p.Page == PageType.Voice.ToString()).ToDictionary(p => p.MediaName, p => p.MediaUrl),
-                VideoEntries = _pageConfiguration.Where(p => p.Page == PageType.Video.ToString()).ToDictionary(p => p.MediaName, p => p.MediaUrl)
+                BioText = _pageConfiguration.First(p => p.Page == PageType.Bio.ToString()).Text
+                //VoiceEntries = _pageConfiguration.Where(p => p.Page == PageType.Voice.ToString()).ToDictionary(p => p.MediaName, p => p.MediaUrl),
+                //VideoEntries = _pageConfiguration.Where(p => p.Page == PageType.Video.ToString()).ToDictionary(p => p.MediaName, p => p.MediaUrl)
             };
+
+            foreach (var voice in _pageConfiguration.Where(p => p.Page == PageType.Voice.ToString()))
+            {
+                view.VoiceEntries.Add(new AdminItem { PageConfigurationIdentifier = voice.PageConfigurationIdentifier, Key = voice.MediaName, Value = voice.MediaUrl });
+            }
+
+            foreach (var video in _pageConfiguration.Where(p => p.Page == PageType.Video.ToString()))
+            {
+                view.VideoEntries.Add(new AdminItem { PageConfigurationIdentifier = video.PageConfigurationIdentifier, Key = video.MediaName, Value = video.MediaUrl });
+            }
+
+            return view;
         }
 
         private void MapToPageConfiguration(AdminView form)
@@ -56,10 +68,36 @@ namespace JeffWeb.Controllers
             _pageConfiguration.First(p => p.Page == PageType.Home.ToString()).Text = form.HomeHtml;
 
 
-
-            foreach (var entry in AdminItems.GetItemEntries(form))
+            foreach (var entry in form.VideoEntries)
             {
-                var value = _pageConfiguration.FirstOrDefault(p => p.Page == PageType.Voice.ToString() && p.MediaName == entry.Key);
+                var value = _pageConfiguration.FirstOrDefault(p => p.Page == PageType.Video.ToString() && p.PageConfigurationIdentifier == entry.PageConfigurationIdentifier);
+
+                if (value == null)
+                {
+                    var configuration = new PageConfiguration
+                    {
+                        Page = PageType.Video.ToString(),
+                        MediaName = entry.Key,
+                        MediaUrl = entry.Value
+                    };
+
+                    _pageConfiguration.Add(configuration);
+
+                    continue;
+                }
+
+                value.MediaName = entry.Key;
+                value.MediaUrl = entry.Value;
+
+                foreach (var pageConfiguration in _pageConfiguration.Where(p => p.Page == PageType.Video.ToString() && form.VideoEntries.All(v => v.Key != p.MediaName)))
+                {
+                    _pageConfiguration.Remove(pageConfiguration);
+                }
+            }
+
+            foreach (var entry in form.VoiceEntries)
+            {
+                var value = _pageConfiguration.FirstOrDefault(p => p.Page == PageType.Voice.ToString() && p.PageConfigurationIdentifier == entry.PageConfigurationIdentifier);
 
                 if (value == null)
                 {
